1a759d4291f015b84f418f2f49beb6d4916cdd93
tbaderts <tbaderts@gmail.com>
2019-12-01 17:53:45 +0100

    bug fix

--- rsocket/rsocket-server/src/main/java/org/example/rsocket/server/ISO8601DateDecorator.java
+++ rsocket/rsocket-server/src/main/java/org/example/rsocket/server/ISO8601DateDecorator.java
@@ -1,6 +1,6 @@
 package org.example.rsocket.server;
 
-import com.fasterxml.jackson.databind.MappingJsonFactory;
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
@@ -9,8 +9,8 @@ import net.logstash.logback.decorate.JsonFactoryDecorator;
 public class ISO8601DateDecorator implements JsonFactoryDecorator {
 
 	@Override
-	public MappingJsonFactory decorate(MappingJsonFactory factory) {
-		ObjectMapper codec = factory.getCodec();
+	public JsonFactory decorate(JsonFactory factory) {
+		ObjectMapper codec = (ObjectMapper) factory.getCodec();
 		codec.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 		return factory;
 	}
